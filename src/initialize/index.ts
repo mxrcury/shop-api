5adc95b0ef2222f6df6e0486da64afde1214fdb2
mxrcury <demetrgonchar@gmail.com>
2023-04-17 09:00:12 -0400

    back connect to db

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -12,6 +12,7 @@ import templatesInit from './templates';
 import { templatesRouter } from '../routes/templates';
 import middlewaresInit from './middlewares';
 import swaggerSettings from '../../swagger-settings';
+import { log } from 'console';
 
 dotenv.config();
 
@@ -31,13 +32,13 @@ const start = async (app: express.Express, port: string | number = 7000) => {
     app.use('*', notFoundRoute);
     app.use(errorHandler);
 
-    // await mongoose.connect(
-    //   process.env.DB.replace('<PASSWORD>', process.env.DB_PASS),
-    //   {
-    //     retryWrites: true,
-    //     monitorCommands: true,
-    //   }
-    // );
+    await mongoose.connect(
+      process.env.DB.replace('<PASSWORD>', process.env.DB_PASS),
+      {
+        retryWrites: true,
+        monitorCommands: true,
+      }
+    );
     console.log(`Successfully connected to MongoDB `);
 
     app.listen(port, () => console.log(`Server started on ${port}`));
